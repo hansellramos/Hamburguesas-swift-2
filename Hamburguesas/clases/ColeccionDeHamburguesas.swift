298757aa3aa764a843f199a76c224eee010a5905
hansellesteban <hansell.ramos@gmail.com>
2016-09-07 10:59:53 -0500

    #FirstCommit

---
+++ Hamburguesas/clases/ColeccionDeHamburguesas.swift
@@ -0,0 +1,17 @@
+//
+//  ColeccionDeHamburguesas.swift
+//  Hamburguesas
+//
+//  Created by Hansel Ramos Osorio on 9/7/16.
+//  Copyright © 2016 Hansel Ramos Osorio. All rights reserved.
+//
+
+import Foundation
+
+class ColeccionDeHamburguesas{
+    
+    func obtenerHambuguesa(){
+        
+    }
+    
+}
