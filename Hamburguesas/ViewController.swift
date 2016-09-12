3a16afadcb2690ebc33ccc57d105eb50b09052b3
hansellesteban <hansell.ramos@gmail.com>
2016-09-12 13:39:21 -0500

    Added Model, button and labels to viewController

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let _paises = ColeccionDePaises()
+    let _hamburguesas = ColeccionDeHamburguesas()
 
+    @IBOutlet weak var lblPais: UILabel!
+    @IBOutlet weak var lblHamburguesa: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func btnPedirHamburguesa(sender: UIButton) {
+        lblPais.text = "País: \(_paises.obtenerPais())"
+        lblHamburguesa.text = "Hamburguesa: \(_hamburguesas.obtenerHambuguesa())"
     }
 
 
